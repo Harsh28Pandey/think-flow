f7e37aa600971bfed42827a6b13764b9597cd390
Harsh Pandey <harsh28.knp@gmail.com>
2026-09-18 11:24:14 +0530

    Update agent.controller.js

--- backend/services/agent/controllers/agent.controller.js
+++ backend/services/agent/controllers/agent.controller.js
@@ -1,9 +1,13 @@
+import { addMessage } from "../config/memory.js"
 import { graph } from "../graph/graph.js"
 import axios from "axios"
 
 export const agent = async (req, res) => {
     try {
         const { prompt, conversationId } = req.body
+
+        // await redis.del(`messages-${conversationId}`)
+
         await axios.post(`${process.env.CHAT_SERVICE}/save-message`, {
             conversationId,
             role: "user",
@@ -16,6 +20,9 @@ export const agent = async (req, res) => {
         })
 
         const response = result.aiResponse
+
+        await addMessage(conversationId, "user", prompt)
+        await addMessage(conversationId, "assistant", response)
 
         await axios.post(`${process.env.CHAT_SERVICE}/save-message`, {
             conversationId,
